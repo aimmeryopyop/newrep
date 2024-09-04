05eaa59fb1c105697966aacbc7bb25164395216d
REJIX1 <164517253+REJIX1@users.noreply.github.com>
2024-09-04 09:00:47 +0800

    Add files via upload

---
+++ creators/resources/js/main.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function closeSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
